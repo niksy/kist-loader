a56f02b683e07f20d2de756acc545823f71e5d18
Ivan Nikolić <niksy5@gmail.com>
2014-02-16 12:09:58 +0100

    Google Maps loader: Expose as jQuery plugin

--- src/gmapsLoader.js
+++ src/gmapsLoader.js
@@ -1,94 +1,100 @@
-/*!
- * JavaScript - loadGoogleMaps( version, apiKey, language, libraries, sensor )
- *
- * - Load Google Maps API using jQuery Deferred.
- *   Useful if you want to only load the Google Maps API on-demand.
- * - Requires jQuery 1.5
- *
- *   UPDATES by Gavin Foley
- * - Tidied JS & made it JSLint compliant
- * - Updated script request to Google Maps API to be protocol relative
- * - Added "sensor" parameter which defaults to false if not present
- *
- * Copyright (c) 2011 Glenn Baker
- * Dual licensed under the MIT and GPL licenses.
- */
-/*globals window, google, jQuery*/
-var loadGoogleMaps = (function ($) {
-    "use strict";
+;(function ( $, window, document, undefined ) {
 
-    var now = $.now(),
-        promise;
+    /*!
+     * JavaScript - loadGoogleMaps( version, apiKey, language, libraries, sensor )
+     *
+     * - Load Google Maps API using jQuery Deferred.
+     *   Useful if you want to only load the Google Maps API on-demand.
+     * - Requires jQuery 1.5
+     *
+     *   UPDATES by Gavin Foley
+     * - Tidied JS & made it JSLint compliant
+     * - Updated script request to Google Maps API to be protocol relative
+     * - Added "sensor" parameter which defaults to false if not present
+     *
+     * Copyright (c) 2011 Glenn Baker
+     * Dual licensed under the MIT and GPL licenses.
+     */
+    /*globals window, google, jQuery*/
+    var loadGoogleMaps = (function ($) {
+        "use strict";
 
-    return function (version, apiKey, language, libraries, sensor) {
-        if (promise) {
-            return promise;
-        }
+        var now = $.now(),
+            promise;
 
-        //Create a Deferred Object
-        var deferred = $.Deferred(),
-            //Declare a resolve function, pass google.maps for the done functions
-            resolve = function () {
-                deferred.resolve(window.google && window.google.maps ? window.google.maps : false);
-            },
-            //global callback name
-            callbackName = "loadGoogleMaps_" + (now++),
+        return function (version, apiKey, language, libraries, sensor) {
+            if (promise) {
+                return promise;
+            }
 
-            // Default Parameters
-            params = $.extend({
-                "sensor": sensor || "false"
-            },
-            apiKey ? {
-                "key": apiKey
-            } : {},
-            language ? {
-                "language": language
-            } : {},
-            libraries ? {
-                "libraries": libraries
-            } : {}
-            );
+            //Create a Deferred Object
+            var deferred = $.Deferred(),
+                //Declare a resolve function, pass google.maps for the done functions
+                resolve = function () {
+                    deferred.resolve(window.google && window.google.maps ? window.google.maps : false);
+                },
+                //global callback name
+                callbackName = "loadGoogleMaps_" + (now++),
 
-        //If google.maps exists, then Google Maps API was probably loaded with the <script> tag
-        if (window.google && window.google.maps) {
-            resolve();
-            //If the google.load method exists, lets load the Google Maps API in Async.
-        } else if (window.google && window.google.load) {
-            window.google.load("maps", version || 3, {
-                "other_params": $.param(params),
-                    "callback": resolve
-            });
-            //Last, try pure jQuery Ajax technique to load the Google Maps API in Async.
-        } else {
-            //Ajax URL params
-            params = $.extend(params, {
-                'callback': callbackName
-            });
+                // Default Parameters
+                params = $.extend({
+                    "sensor": sensor || "false"
+                },
+                apiKey ? {
+                    "key": apiKey
+                } : {},
+                language ? {
+                    "language": language
+                } : {},
+                libraries ? {
+                    "libraries": libraries
+                } : {}
+                );
 
-            //Declare the global callback
-            window[callbackName] = function () {
+            //If google.maps exists, then Google Maps API was probably loaded with the <script> tag
+            if (window.google && window.google.maps) {
                 resolve();
+                //If the google.load method exists, lets load the Google Maps API in Async.
+            } else if (window.google && window.google.load) {
+                window.google.load("maps", version || 3, {
+                    "other_params": $.param(params),
+                        "callback": resolve
+                });
+                //Last, try pure jQuery Ajax technique to load the Google Maps API in Async.
+            } else {
+                //Ajax URL params
+                params = $.extend(params, {
+                    'callback': callbackName
+                });
 
-                //Delete callback
-                setTimeout(function () {
-                    try {
-                        delete window[callbackName];
-                    } catch (e) {}
-                }, 20);
-            };
+                //Declare the global callback
+                window[callbackName] = function () {
+                    resolve();
 
-            //Can't use the jXHR promise because 'script' doesn't support 'callback=?'
-            $.ajax({
-                dataType: 'script',
-                data: params,
-                url: '//maps.googleapis.com/maps/api/js'
-            });
+                    //Delete callback
+                    setTimeout(function () {
+                        try {
+                            delete window[callbackName];
+                        } catch (e) {}
+                    }, 20);
+                };
 
-        }
+                //Can't use the jXHR promise because 'script' doesn't support 'callback=?'
+                $.ajax({
+                    dataType: 'script',
+                    data: params,
+                    url: '//maps.googleapis.com/maps/api/js'
+                });
 
-        promise = deferred.promise();
+            }
 
-        return promise;
-    };
+            promise = deferred.promise();
 
-})(jQuery);
+            return promise;
+        };
+
+    })($);
+
+    $.gmapsApiLoader = loadGoogleMaps;
+
+})( jQuery, window, document );
