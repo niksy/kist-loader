705b1324f1486ad31dd90c124c0d8525e3e9ab60
Ivan Nikolić <niksy5@gmail.com>
2014-02-16 13:41:56 +0100

    Modify tests

--- test/multiGetScript.js
+++
@@ -1,8 +0,0 @@
-var deps = [ "http://documentcloud.github.com/backbone/backbone-min.js",
-             "http://documentcloud.github.com/underscore/underscore-min.js" ];
-
-
-$.multiGetScript( deps, function( jqXhr ) {
-	console.log( jqXhr );
-	console.log( [ _, Backbone ] );
-});
